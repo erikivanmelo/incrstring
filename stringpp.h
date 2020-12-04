aab0c65e50f3519d7cee12c72449f0f43ad88682
Erik Ivan Melo <megaerikivan2@gmail.com>
2020-12-03 21:47:29 -0300

    fixed some problems
    now...
    strrem: you can remove characters from index 0.
    strsplit: if you pass a string that does not contain the separator as a parameter, it saves you an array with a single element that will contain the entire string.
    general: when using the library in c++ there will be no problems of multiple definitions.

--- stringpp.h
+++ stringpp.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /* Returns 1 if the string is empty. */
 #define strempty(str)  (strcmp(str,"")==0)
 
@@ -58,7 +57,7 @@
 
 
 /* Inserts a substring in a specific point. */
-void strins(char* str,long x,const char* substr){
+static void strins(char* str,long x,const char* substr){
 	const size_t substr_len = strlen(substr);
 	const size_t len = strlen(str);
 
@@ -73,16 +72,16 @@ void strins(char* str,long x,const char* substr){
 }
 
 /* Inserts a character in a specific point. */
-void strchins(char* str,long x,const char ch ){ const char *temp = &ch; strins(str,x,temp) ;}
+static void strchins(char* str,long x,const char ch ){ const char *temp = &ch; strins(str,x,temp) ;}
 
 /* Inserts a character to the left of the string. */
-void strchinsl(char* str,const char ch 	    ){ const char *temp = &ch; strinsl(str,temp)  ;}
+static void strchinsl(char* str,const char ch 	    ){ const char *temp = &ch; strinsl(str,temp)  ;}
 
 /* Inserts a character to the right of the string. */
-void strchinsr(char* str,const char ch 	    ){ const char *temp = &ch; strcat(str,temp)   ;}
+static void strchinsr(char* str,const char ch 	    ){ const char *temp = &ch; strcat(str,temp)   ;}
 
 /* Returns the first position of a substring analyzing it from left to right, starting from the incident 'i'. */
-long strposlr(const char *str,const char *substr, long i){
+static long strposlr(const char *str,const char *substr, long i){
 	if(strempty(str)) return -1;
 	size_t substr_len = strlen(substr);
 	const size_t len = (strlen(str)-substr_len)+1;
@@ -119,7 +118,7 @@ long strposlr(const char *str,const char *substr, long i){
 }
 
 /* Returns the first position of a substring analyzing it from right to left, starting from the incident 'i'. */
-long strposrl(char *str,const char *substr, long i){
+static long strposrl(char *str,const char *substr, long i){
 	if ( strempty(str) ) return -1;
 	if ( strempty(substr) ) return -2;
 	if ( strlen(substr) > strlen(str) ) return -2;
@@ -156,7 +155,7 @@ long strposrl(char *str,const char *substr, long i){
 }
 
 /* Returns the number of times a substring appears. */
-size_t strcount(const char *str, const char *word){
+static size_t strcount(const char *str, const char *word){
 	size_t count = 0,i=0;
 	const size_t word_len = strlen(word);
 	if ( strempty(str) || strempty(word)) return 0;
@@ -174,20 +173,20 @@ size_t strcount(const char *str, const char *word){
 }
 
 /* Removes the character from a string that is in a specific index. */
-char strrem(char* str, long i){
+static char strrem(char* str, long i){
 	const size_t len = strlen(str);
 	if (strempty(str)) return -1;
 	if (i >= len)return -2;
 	if (i < 0 && i*-1 >= len)return -2;
-	if (i<0) i = len-i;else i--;
-	while( i++ < len)
+	if (i<0) i = len-i;
+	for (; i < len; ++i)
 		str[i] = str[i+1];
 	str[i]='\0';
 	return 1;
 }
 
 /* Replaces a substring with another substring. */
-void strrep(char *str,const char *a,const char *b){
+static void strrep(char *str,const char *a,const char *b){
 	size_t i2,i;
 	const size_t len   = strlen(str);
 	const size_t a_len = i2 = strlen(a);
@@ -222,7 +221,7 @@ void strrep(char *str,const char *a,const char *b){
 }
 
 /* Replaces the first occurrence of the substring with another substring. */
-void strfrep(char *str,const char *a,const char *b){
+static void strfrep(char *str,const char *a,const char *b){
 	//if (str[0] != a[0]) retrn;
 
 	size_t i2,i;
@@ -249,7 +248,7 @@ void strfrep(char *str,const char *a,const char *b){
 }
 
 /* Replaces the last occurrence of the substring with another substring. */
-void strlrep(char *str,const char *a,const char *b){
+static void strlrep(char *str,const char *a,const char *b){
 	size_t i2,i;
 	const size_t a_len = i2 = strlen(a);
 	const size_t b_len = strlen(b);
@@ -275,7 +274,7 @@ void strlrep(char *str,const char *a,const char *b){
 }
 
 /* Separates a string based on a separator and save the result in a matrix. And return the number of substrings that resulted from that separation returns. */
-size_t strsplit(const char *str,const char *sep,const size_t w,char ***arr){
+static size_t strsplit(const char *str,const char *sep,const size_t w,char ***arr){
 	long i = -1,x = -1;
 	size_t y = 0;
 	
@@ -286,7 +285,8 @@ size_t strsplit(const char *str,const char *sep,const size_t w,char ***arr){
 	if ( strfpos(str,sep) < 0 ){
 		aux[0] = (char*)malloc(strlen(str)+1 * sizeof(char) );
 		strcpy(aux[0],str);
-		return 0;
+		*arr = aux;
+		return 1;
 	}else{
 		aux[0] = (char*)malloc(w * sizeof(char) );
 	}
